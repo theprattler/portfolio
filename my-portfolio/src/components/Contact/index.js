dafb4786f8769ae85ff56e7ac4e27948d20c9214
The Prattler <jmichaelsmith@juno.com>
2022-04-18 15:27:12 -0400

    setup nodemailer for contact form, not complete

--- my-portfolio/src/components/Contact/index.js
+++ my-portfolio/src/components/Contact/index.js
@@ -3,6 +3,7 @@ import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
   const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+  const [status, setStatus] = useState('Submit');
   const { name, email, message } = formState;
   const [errorMessage, setErrorMessage] = useState('');
 
@@ -27,10 +28,26 @@ function ContactForm() {
     }
   }
 
-  function handleSubmit(e) {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(formState);
-  }
+    setStatus('Sending...');
+    const { name, email, message } = e.target.elements;
+    let details = {
+      name: name.value,
+      email: email.value,
+      message: message.value
+    };
+    let response = await fetch('http://localhost:5000/contact', {
+      method: 'POST',
+      headers: {
+        'Contact-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(details)
+    });
+    setStatus('Submit');
+    let result = await response.json();
+    alert(result.status);
+  };
 
   return (
     <section>
@@ -38,22 +55,22 @@ function ContactForm() {
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
+          <input type="text" defaultValue={name} onBlur={handleChange} name="name" required />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" defaultValue={email} onBlur={handleChange} name="email" />
+          <input type="email" defaultValue={email} onBlur={handleChange} name="email" required />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" />
+          <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" required />
         </div>
         {errorMessage && (
           <div>
             <p className='error-text'>{errorMessage}</p>
           </div>
         )}
-        <button data-testid='button' type="submit">Submit</button>
+        <button data-testid='button' type="submit">{status}</button>
       </form>
       <div>
         <ul>
