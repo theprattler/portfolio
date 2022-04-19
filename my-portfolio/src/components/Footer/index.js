aeb74e5bb0fd630f41846c4e7a9e746612689999
The Prattler <jmichaelsmith@juno.com>
2022-04-19 15:01:28 -0400

    stylized footer

--- my-portfolio/src/components/Footer/index.js
+++ my-portfolio/src/components/Footer/index.js
@@ -6,15 +6,22 @@ import emailIcon from '../../assets/icons/email-48.png';
 function Footer() {
   return (
     <footer>
-      <a href="https://github.com/theprattler" target='_blank' rel='noreferrer'>
-        <img src={githubIcon} alt='link to github profile' />
-      </a>
-      <a href="https://www.linkedin.com/in/j-michael-smith-917230220/" target='_blank' rel='noreferrer'>
-        <img src={linkedinIcon} alt='link to linkedin profile' />
-      </a>
-      <a href='mailto:jmichaelsmith.writer@gmail.com'>
-        <img src={emailIcon} alt='contact via email' />
-      </a>
+      <div className='footLinkContainer'>
+        
+        <a href="https://github.com/theprattler" target='_blank' rel='noreferrer'>
+          <img src={githubIcon} alt='link to github profile' />
+        </a>
+        
+        
+        <a href="https://www.linkedin.com/in/j-michael-smith-917230220/" target='_blank' rel='noreferrer'>
+          <img src={linkedinIcon} alt='link to linkedin profile' />
+        </a>
+        
+        <a href='mailto:jmichaelsmith.writer@gmail.com'>
+          <img src={emailIcon} alt='contact via email' />
+        </a>
+        
+      </div>
     </footer>
   )
 }
