1fa82bf6bd412faa67ebe89f2df955060e6efe0c
The Prattler <jmichaelsmith@juno.com>
2022-04-19 16:09:13 -0400

    stylized about me page

--- my-portfolio/src/components/About/index.js
+++ my-portfolio/src/components/About/index.js
@@ -3,18 +3,23 @@ import aboutImage from '../../assets/images/test-two.jpg';
 
 function About() {
   return (
-    <section>
-      <h2 id="about">A Little About Me</h2>
-      <div>
-        <img src={aboutImage} style={{ width: '20%' }} alt='black and white of J Michael Smith'></img>
-      </div>
-      <div>
-        <p>
-          I am a full-stack web developer utilizing a background in <a href='https://www.youtube.com/channel/UC3lxkRvIOZASJuvV5UQY9OQ' target='_blank' rel='noreferrer'>music</a> and <a href='https://www.amazon.com/Children-Calm-J-Michael-Smith-ebook/dp/B01BWI5EUO/ref=sr_1_1?crid=3AEFNJSEVCG8Z&keywords=the+children+of+calm+j+michael+smith&qid=1643567744&sprefix=the+children+of+calm+j+michael+smith%2Caps%2C86&sr=8-1' target='_blank' rel='noreferrer'>writing</a> to creatively build a user-enjoyed web experience. I received my certificate for completing <a href='https://bootcamp.unc.edu/coding/' target='_blank' rel='noreferrer'>The Coding Bootcamp at UNC Chapel Hill</a> in March 2022. Known as loyal and hard-working, I strive to create an experience that is both user-friendly and visually appealing for each project. It excites me to use my skills in a setting that values teamwork and quality output.
-          <br></br>
-          <br></br>
-          I currently reside in Clayton, North Carolina, with my wife and two children.
-        </p>
+    <section className='sectionContainer'>
+      <h2>A Little About Me</h2>
+      <div className='aboutContainer'>
+        <div className='imageContainer'>
+          <img src={aboutImage} className='profPic' alt='black and white of J Michael Smith'></img>
+          <div className='profPicOverlay'>
+            <div className='overlayText'>Peek-a-boo</div>
+          </div>
+        </div>
+        <div>
+          <p className='aboutText'>
+            I am a full-stack web developer utilizing a background in <a href='https://www.youtube.com/channel/UC3lxkRvIOZASJuvV5UQY9OQ' target='_blank' rel='noreferrer'>music</a> and <a href='https://www.amazon.com/Children-Calm-J-Michael-Smith-ebook/dp/B01BWI5EUO/ref=sr_1_1?crid=3AEFNJSEVCG8Z&keywords=the+children+of+calm+j+michael+smith&qid=1643567744&sprefix=the+children+of+calm+j+michael+smith%2Caps%2C86&sr=8-1' target='_blank' rel='noreferrer'>writing</a> to creatively build a user-enjoyed web experience. I received my certificate for completing <a href='https://bootcamp.unc.edu/coding/' target='_blank' rel='noreferrer'>The Coding Bootcamp at UNC Chapel Hill</a> in March 2022. Known as loyal and hard-working, I strive to create an experience that is both user-friendly and visually appealing for each project. It excites me to use my skills in a setting that values teamwork and quality output.
+            <br></br>
+            <br></br>
+            I currently reside in Clayton, North Carolina, with my wife and two children.
+          </p>
+        </div>
       </div>
     </section>
   )
--- my-portfolio/src/components/Footer/index.js
+++ my-portfolio/src/components/Footer/index.js
@@ -7,20 +7,15 @@ function Footer() {
   return (
     <footer>
       <div className='footLinkContainer'>
-        
         <a href="https://github.com/theprattler" target='_blank' rel='noreferrer'>
           <img src={githubIcon} alt='link to github profile' />
         </a>
-        
-        
         <a href="https://www.linkedin.com/in/j-michael-smith-917230220/" target='_blank' rel='noreferrer'>
           <img src={linkedinIcon} alt='link to linkedin profile' />
         </a>
-        
         <a href='mailto:jmichaelsmith.writer@gmail.com'>
           <img src={emailIcon} alt='contact via email' />
         </a>
-        
       </div>
     </footer>
   )
